619c93623b580eb3fffce2df445d94a9cb8c5667
JMDahle <47642064+jmdahle@users.noreply.github.com>
2020-10-20 19:30:30 -0500

    trying YET  again to get api routes to respond

--- server.js
+++ server.js
@@ -15,7 +15,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
+const Scores = require("../database/models/scores.js");
+
+// return all scores
+router.get("/api/scores/all", (req, res) => {
+  console.log("Hey, JD!  We got a request for our list of songs.");
+  Scores.find({})
+    .then(dbScores => {
+      // console.log("JD, I got the goods.");
+      // console.log("Take a look...", dbScores.length);
+      res.json(dbScores);
+    })
+    .catch(err => {
+      // console.log("Uh oh, JD.  Deal with this:", err);
+      res.status(400).json(err);
+    });
+});
 
 
 
